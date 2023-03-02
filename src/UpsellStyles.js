a1b5ab4734cd321ac497bce6cc15007866688351
trickett <trickett@linkedin.com>
2023-03-01 17:42:40 -0800

    Update upsell class names to be potentially more intuitive

--- src/UpsellStyles.js
+++ src/UpsellStyles.js
@@ -3,19 +3,18 @@ function UpsellStyles() {
     <style>
       {
         `
-        .promo-bottom-sheet__card {
+        .container {
           display: flex; 
           z-index: 10; 
           padding: 16px; 
           text-align: left; 
           flex-direction: column; 
-          width: -webkit-fill-available;; 
           background-color: #ffffff;
           border-top-left-radius: 20px;
           border-top-right-radius: 20px;
         }
   
-        .promo-bottom-sheet__card-headline {
+        .title {
           margin-bottom: 16px;
           font-family: sans-serif;
           font-weight: 600; 
@@ -25,12 +24,12 @@ function UpsellStyles() {
         }
   
         .promo-bottom-sheet__card-action {
-          display: flex; 
-          align-items: center; 
+          display: flex;
+          align-items: center;
           margin-bottom: 20px;
         }
   
-        .promo-bottom-sheet__card-icon {
+        .icon {
           display: inline-block; 
           margin-right: 16px; 
           flex-shrink: 0; 
@@ -39,50 +38,25 @@ function UpsellStyles() {
           color: #0a66c2;
         }
   
-        .promo-bottom-sheet__card-icon-svg {
+        .svg {
           height: 20px;
           width: 20px;
         }
   
-        .promo-bottom-sheet__card-text {
+        p {
           font-family: sans-serif;
           color: #000000e6;
           flex-grow: 1;
+          margin: 0;
         }
   
-        .promo-bottom-sheet__action {
-          box-shadow: 0 0 0 1px #00000000;
-          background-color: #0a66c2;
-          color: #ffffff;
-          cursor: pointer;
-          text-decoration: none;
-          height: min-content;
-          min-height: 32px;
-          border-radius: 24px;
-          padding-top: 7px;
-          padding-bottom: 7px;
-          padding-left: 16px;
-          padding-right: 16px;
-          text-align: center;
-          font-family: sans-serif;
-          font-size: 14px;
-          font-weight: 600;
-          min-width: 110px;
-          border: 0;
-          vertical-align: baseline;
-          box-sizing: border-box;
-        }
-  
-        .promo-bottom-sheet__card-action {
+        .button-container {
           display: flex; 
           align-items: center; 
           margin-bottom: 20px;
         }
-  
-        .promo-bottom-sheet__dismiss {
-          box-shadow: 0 0 0 1px #000000bf;
-          background-color: #00000000;
-          color: #000000bf;
+
+        .button {
           cursor: pointer;
           text-decoration: none;
           height: min-content;
@@ -96,9 +70,23 @@ function UpsellStyles() {
           font-family: sans-serif;
           font-size: 14px;
           font-weight: 600;
-          border: 1px solid transparent;
           min-width: 110px;
-        }`
+        }
+
+        .open-app-button {
+          box-shadow: 0 0 0 1px #00000000;
+          background-color: #0a66c2;
+          color: #ffffff;
+          border: 0;
+        }
+  
+        .continue-button {
+          box-shadow: 0 0 0 1px #000000bf;
+          background-color: #00000000;
+          color: #000000bf;
+          border: 1px solid transparent;
+        }
+        `
       }
     </style>
   );
